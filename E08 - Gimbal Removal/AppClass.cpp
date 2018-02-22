808070717e3cfdf723fdb358596bb05cb57d816d
TIMSTABLET\Tim <ascenciot123@gmail.com>
2018-02-22 16:50:23 -0500

    -_- deleted. Somethings terribly wrong I have a lot of issues pushing and pulling im scared

--- E08 - Gimbal Removal/AppClass.cpp
+++ E08 - Gimbal Removal/AppClass.cpp
@@ -30,7 +30,7 @@ void Application::Display(void)
 	m_m4Model = glm::rotate(m_m4Model, m_v3Rotation.y, vector3(0.0f, 1.0f, 0.0f));
 	m_m4Model = glm::rotate(m_m4Model, m_v3Rotation.z, vector3(0.0f, 0.0f, 1.0f));
 
-	// orientation -_-
+	// orientation
 	m_qOrientation = m_qOrientation * glm::angleAxis(1.0f, vector3(1.0f));
 	
 	m_pMesh->Render(m4Projection, m4View, ToMatrix4(m_qOrientation));
