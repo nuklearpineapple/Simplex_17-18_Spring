f0256af8df3565725cf57b60b8dd1aa20368ef27
TIMSTABLET\Tim <ascenciot123@gmail.com>
2018-04-08 00:19:46 -0400

    A05 TANAT IS AWESOME !!!!!!!!

--- A05 - SAT/MyRigidBody.cpp
+++ A05 - SAT/MyRigidBody.cpp
@@ -285,14 +285,25 @@ uint MyRigidBody::SAT(MyRigidBody* const a_pOther)
 	OBB b;
 
 	// assign c centers
-	a.c = this->m_v3Center;
-	b.c = a_pOther->m_v3Center;
+	a.c = this->GetCenterGlobal();
+	b.c = a_pOther->GetCenterGlobal();
 
-	// assign u axes , [0] = x axis , [2] = z axis
-	a.u[0] = vector3(this->m_fRadius, this->m_v3MaxG.y, this->m_v3MaxG.z);
-	a.u[2] = vector3(this->m_v3MaxG.x, this->m_v3MaxG.y, this->m_fRadius);
-	b.u[0] = vector3(a_pOther->m_fRadius, a_pOther->m_v3MaxG.y, a_pOther->m_v3MaxG.z);
-	b.u[2] = vector3(a_pOther->m_v3MaxG.x, a_pOther->m_v3MaxG.y, a_pOther->m_fRadius);
+	
+	vector4 xToWorld = this->m_m4ToWorld * vector4(1, 0, 0, 0);
+	a.u[0] = vector3(xToWorld);
+	vector4 yToWorld = this->m_m4ToWorld * vector4(0, 1, 0, 0);
+	a.u[1] = vector3(yToWorld);
+	vector4 zToWorld = this->m_m4ToWorld * vector4(0, 0, 1, 0);
+	a.u[2] = vector3(zToWorld);
+
+
+
+	xToWorld = a_pOther->m_m4ToWorld * vector4(1, 0, 0, 0);
+	b.u[0] = vector3(xToWorld);
+	yToWorld = a_pOther->m_m4ToWorld * vector4(0, 1, 0, 0);
+	b.u[1] = vector3(yToWorld);
+	zToWorld = a_pOther->m_m4ToWorld * vector4(0, 0, 1, 0);
+	b.u[2] = vector3(zToWorld);
 
 	// assign e half_widths
 	a.e = this->m_v3HalfWidth;
@@ -308,11 +319,11 @@ uint MyRigidBody::SAT(MyRigidBody* const a_pOther)
 	// Compute translation vector t
 	vector3 t = b.c - a.c;
 	// Bring translation into a's coordinate frame
-	t = vector3(glm::dot(t, a.u[0]), glm::dot(t, a.u[2]), glm::dot(t, a.u[2]));
+	t = vector3(glm::dot(t, a.u[0]), glm::dot(t, a.u[1]), glm::dot(t, a.u[2]));
 
 	// Compute common subexpressions. Add in an epsilon term to
 	// counteract arithmetic errors when two edges are parallel and
-	// their cross product is (near) null (see text for details)
+	// their cross product is (near) null
 	for (int i = 0; i < 3; i++)
 		for (int j = 0; j < 3; j++)
 			AbsR[i][j] = glm::abs(R[i][j]) + EPSILON;
@@ -321,65 +332,65 @@ uint MyRigidBody::SAT(MyRigidBody* const a_pOther)
 	for (int i = 0; i < 3; i++) {
 		ra = a.e[i];
 		rb = b.e[0] * AbsR[i][0] + b.e[1] * AbsR[i][1] + b.e[2] * AbsR[i][2];
-		if (glm::abs(t[i]) > ra + rb) return 0;
+		if (glm::abs(t[i]) > ra + rb) return eSATResults::SAT_AX;
 	}
 
 	// Test axes L = B0, L = B1, L = B2
 	for (int i = 0; i < 3; i++) {
 		ra = a.e[0] * AbsR[0][i] + a.e[1] * AbsR[1][i] + a.e[2] * AbsR[2][i];
 		rb = b.e[i];
-		if (glm::abs(t[0] * R[0][i] + t[1] * R[1][i] + t[2] * R[2][i]) > ra + rb) return 0;
+		if (glm::abs(t[0] * R[0][i] + t[1] * R[1][i] + t[2] * R[2][i]) > ra + rb) return eSATResults::SAT_AX;
 	}
 
 	#pragma region Extra Testing
 	// Test axis L = A0 x B0
 	ra = a.e[1] * AbsR[2][0] + a.e[2] * AbsR[1][0];
 	rb = b.e[1] * AbsR[0][2] + b.e[2] * AbsR[0][1];
-	if (glm::abs(t[2] * R[1][0] - t[1] * R[2][0]) > ra + rb) return 0;
+	if (glm::abs(t[2] * R[1][0] - t[1] * R[2][0]) > ra + rb) return eSATResults::SAT_AX;
 
 	// Test axis L = A0 x B1
 	ra = a.e[1] * AbsR[2][1] + a.e[2] * AbsR[1][1];
 	rb = b.e[0] * AbsR[0][2] + b.e[2] * AbsR[0][0];
-	if (glm::abs(t[2] * R[1][1] - t[1] * R[2][1]) > ra + rb) return 0;
+	if (glm::abs(t[2] * R[1][1] - t[1] * R[2][1]) > ra + rb) return eSATResults::SAT_AX;
 
 	// Test axis L = A0 x B2
 	ra = a.e[1] * AbsR[2][2] + a.e[2] * AbsR[1][2];
 	rb = b.e[0] * AbsR[0][1] + b.e[1] * AbsR[0][0];
-	if (glm::abs(t[2] * R[1][2] - t[1] * R[2][2]) > ra + rb) return 0;
+	if (glm::abs(t[2] * R[1][2] - t[1] * R[2][2]) > ra + rb) return eSATResults::SAT_AX;
 
 	// Test axis L = A1 x B0
 	ra = a.e[0] * AbsR[2][0] + a.e[2] * AbsR[0][0];
 	rb = b.e[1] * AbsR[1][2] + b.e[2] * AbsR[1][1];
 
-	if (glm::abs(t[0] * R[2][0] - t[2] * R[0][0]) > ra + rb) return 0;
+	if (glm::abs(t[0] * R[2][0] - t[2] * R[0][0]) > ra + rb) return eSATResults::SAT_AX;
 
 	// Test axis L = A1 x B1
 	ra = a.e[0] * AbsR[2][1] + a.e[2] * AbsR[0][1];
 	rb = b.e[0] * AbsR[1][2] + b.e[2] * AbsR[1][0];
-	if (glm::abs(t[0] * R[2][1] - t[2] * R[0][1]) > ra + rb) return 0;
+	if (glm::abs(t[0] * R[2][1] - t[2] * R[0][1]) > ra + rb) return eSATResults::SAT_AX;
 
 	// Test axis L = A1 x B2
 	ra = a.e[0] * AbsR[2][2] + a.e[2] * AbsR[0][2];
 	rb = b.e[0] * AbsR[1][1] + b.e[1] * AbsR[1][0];
-	if (glm::abs(t[0] * R[2][2] - t[2] * R[0][2]) > ra + rb) return 0;
-
+	if (glm::abs(t[0] * R[2][2] - t[2] * R[0][2]) > ra + rb) return eSATResults::SAT_AX;
+	
 	// Test axis L = A2 x B0
 	ra = a.e[0] * AbsR[1][0] + a.e[1] * AbsR[0][0];
 	rb = b.e[1] * AbsR[2][2] + b.e[2] * AbsR[2][1];
-	if (glm::abs(t[1] * R[0][0] - t[0] * R[1][0]) > ra + rb) return 0;
+	if (glm::abs(t[1] * R[0][0] - t[0] * R[1][0]) > ra + rb) return eSATResults::SAT_AX;
 
 	// Test axis L = A2 x B1
 	ra = a.e[0] * AbsR[1][1] + a.e[1] * AbsR[0][1];
 	rb = b.e[0] * AbsR[2][2] + b.e[2] * AbsR[2][0];
-	if (glm::abs(t[1] * R[0][1] - t[0] * R[1][1]) > ra + rb) return 0;
+	if (glm::abs(t[1] * R[0][1] - t[0] * R[1][1]) > ra + rb) return eSATResults::SAT_AX;
 
 	// Test axis L = A2 x B2
 	ra = a.e[0] * AbsR[1][2] + a.e[1] * AbsR[0][2];
 	rb = b.e[0] * AbsR[2][1] + b.e[1] * AbsR[2][0];
-	if (glm::abs(t[1] * R[0][2] - t[0] * R[1][2]) > ra + rb) return 0;
+	if (glm::abs(t[1] * R[0][2] - t[0] * R[1][2]) > ra + rb) return eSATResults::SAT_AX;
 	#pragma endregion
 	
 	// Since no separating axis is found, the OBBs must be intersecting
-	return 1;
+	return eSATResults::SAT_NONE;
 }
 
